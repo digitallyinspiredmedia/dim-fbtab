ad6166e58cc9d3fe6e7f597129003d182c82bc53
S.I.Kavinraj <sikavinraj@gmail.com>
2016-09-28 14:43:58 +0530

    careers-fbtab

---
+++ index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Careers | Digitally Inspired Media </title>
+    <link href="style.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+<!-- working at -->
+      <section class="pages-section" id="working">
+        <div class="grid">
+          <h2 class="page-section__heading">Working at Digitally Inspired Media</h2>
+          <p class="lead">We are a team of incredibly diverse inspirers who breathe creativity. We are digital media mavericks and work hard to keep up with the title. And when it comes to partying, no one does it better than us! Have a look for yourself.</p>
+          <iframe src="https://www.youtube-nocookie.com/embed/videoseries?list=PL1xxjk3_VEyoShGEUUs5392xWaBiTGpD0" frameborder="0" allowfullscreen></iframe>
+          </div>
+      </section>
+    <!-- working at -->
+    <!-- opening -->
+      <section class="pages-section" id="opening">
+        <h2 class="page-section__heading">Open Roles</h2>
+        <p class="lead">Become an inspirer now!</p>
+        <article>
+          <div class="job-card">
+              <h4> Senior Client Servicing Executive </h4>
+              <p class="job-lead">Do you have the gift of the gab? If you can talk, convince and pitch like a pro, then we want you!</p>
+              <div class="job-action">
+                <div class="apply-trigger">
+                  <a href="senior-client-servicing-executive.html" class="apply-btn"> Apply Now </a>
+                </div>
+              </div>
+          </div>
+        </article>
+        <article>
+          <div class="job-card">
+              <h4>  Client Servicing Executive </h4>
+              <p class="job-lead">Are you capable of maintaining the linear function of work while executing smooth execution of campaigns?</p>
+                <div class="job-action">
+                <div class="apply-trigger">
+                  <a href="client-servicing-executive.html" class="apply-btn"> Apply Now </a>
+                </div>
+              </div>
+          </div>
+        </article>
+        <article>
+          <div class="job-card">
+              <h4> Digital Advertising Specialist </h4>
+              <p class="job-lead">Will you commit to an idea and see it to fruition through strategy, management, and execution?If it's a yes, join the team!</p>
+                <div class="job-action">
+                <div class="apply-trigger">
+                  <a href="digital-advertising-specialist.html" class="apply-btn"> Apply Now </a>
+                </div>
+              </div>
+          </div>
+        </article>
+        <article>
+          <div class="job-card">
+              <h4> Senior Graphic Designer </h4>
+              <p class="job-lead">Are you a wizard with Adobe Illustrator, Photoshop and the creative suite from Adobe? Can you make viewers spellbound with your designs? </p>
+              <div class="job-action">
+                <div class="apply-trigger">
+                  <a href="senior-graphic-designer.html" class="apply-btn"> Apply Now </a>
+                </div>
+              </div>
+          </div>
+        </article>
+        <article>
+          <div class="job-card">
+              <h4> Digital Strategist </h4>
+              <p class="job-lead">Every game plan has a great strategist behind it. Are you have the right mix of cool and chaos to think Digital and outside the box? If you do, you're perfect for our team.</p>
+              <div class="job-action">
+                <div class="apply-trigger">
+                  <a href="digital-strategist.html" class="apply-btn"> Apply Now </a>
+                </div>
+              </div>
+          </div>
+        </article>
+        <article>
+          <div class="job-card">
+              <h4> HR Manager </h4>
+              <p class="job-lead">Are you a wizard with Hiring? </p>
+              <div class="job-action">
+                <div class="apply-trigger">
+                  <a href="#" class="apply-btn"> Apply Now </a>
+                </div>
+              </div>
+          </div>
+        </article>
+      </section>
+    <!-- opening -->
+    <!-- footer -->
+      <footer>
+        <p>&copy; 2016 Digitally Inspired Media</p>
+        <div class="social-link">
+          <ul class="social-icon">
+            <li class="facebook">
+              <a href="https://www.facebook.com/DigitallyInspiredMedia" >Facebook</a>
+            </li>
+            <li class="google">
+              <a href="https://plus.google.com/+DigitallyInspiredMedia" >Google Plus</a>
+            </li>
+            <li class="twitter">
+              <a href="https://twitter.com/digital_inspire" >Twitter</a>
+            </li>
+            <li class="behance">
+              <a href="https://www.behance.net/digitallyinspired" >Behance</a>
+            </li>
+           <li class="youtube">
+              <a href="https://www.youtube.com/user/DigitallyInspiredTV" >Youtube</a>
+            </li>
+            <li class="medium">
+              <a href="https://medium.com/@digitallyinspiredmedia" >Medium</a>
+            </li>
+            <li class="instagram">
+              <a href="https://www.instagram.com/digitallyinspiredmedia_/" >Instagram</a>
+            </li>
+            <li class="linkedin">
+              <a href="https://www.linkedin.com/company/2634160" >Linkedin</a>
+            </li>
+            <li class="pinterest">
+              <a href="https://in.pinterest.com/digitalinspire/" >Pintrest</a>
+            </li> 
+          </ul>
+        </div>
+      </footer>
+    <!-- footer -->
+  <!-- container -->
+  </body>
+</html>
